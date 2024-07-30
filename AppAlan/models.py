08c4d8a5e3a3f89ccc9683357338e07a8ee661d7
Alan-69 <alanpablo69@gmail.com>
2024-07-29 23:08:23 -0300

    EntregaProject_1

--- AppAlan/models.py
+++ AppAlan/models.py
@@ -4,19 +4,31 @@ from django.db import models
 class Curso(models.Model):
     nombre = models.CharField(max_length=40)
     camada = models.IntegerField()
+    
+    def __str__(self):
+        return "{} | ID: {}".format(self.nombre, self.id)
 
 class Estudiante(models.Model):
     nombre = models.CharField(max_length=40)
     apellido = models.CharField(max_length=20)
     email = models.EmailField(max_length=40)
 
+    def __str__(self):
+        return "Nombre completo del alumno: {} {} | Dirección Email: {}".format(self.nombre,self.apellido,self.email)
+
 class Profesor(models.Model):
     nombre = models.CharField(max_length=40)
     apellido = models.CharField(max_length=20)
     email = models.EmailField(max_length=40)
     apellido = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return "Nombre completo del profesor: {} {}".format(self.nombre,self.apellido)
 
 class Entregable(models.Model):
     nombre = models.CharField(max_length=30)
     fecha_de_entrega = models.DateField()
     entregado = models.BooleanField()
+
+    def __str__(self):
+        return "ID de entrega: {}  |  (click para más información)".format(self.nombre)
